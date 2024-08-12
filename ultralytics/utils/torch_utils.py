4af68a7dbdaec4678fa8d7828f6c420e9029edc9
MichaelHamonangan <hamonanganmichael@mail.ugm.ac.id>
2024-08-12 20:05:15 +0700

    Attention for Segmentation

--- ultralytics/utils/torch_utils.py
+++ ultralytics/utils/torch_utils.py
@@ -707,3 +707,9 @@ class EarlyStopping:
                 f"i.e. `patience=300` or use `patience=0` to disable EarlyStopping."
             )
         return stop
+
+def make_divisible(x, divisor):
+    """Returns nearest x divisible by divisor."""
+    if isinstance(divisor, torch.Tensor):
+        divisor = int(divisor.max())  # to int
+    return math.ceil(x / divisor) * divisor
